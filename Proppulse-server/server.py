b183be344c76ef33fe8a4098f2876863425a272e
KGX1000 <kgouin8116@gmail.com>
2026-03-09 23:54:19 -0500

    Update server.py

--- Proppulse-server/server.py
+++ Proppulse-server/server.py
@@ -1,6 +1,7 @@
 from nba_api.stats.endpoints import playergamelog
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -15,4 +16,5 @@ def get_log(player_id):
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
